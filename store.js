5498a7edccdd2d94ffece2115ada9aeb8301775a
pacokwon <haechank@gmail.com>
2020-04-02 18:10:41 +0900

    add - move long json constants to a separate store.js file

---
+++ store.js
@@ -0,0 +1,83 @@
+const captions = [
+    {
+        begin: '00:00:00,000',
+        end: '00:00:03,230',
+        script: 'And looking at the history a little bit,'
+    },
+    {
+        begin: '00:00:03,530',
+        end: '00:00:10,340',
+        script: 'all the way back in 1981, was the first time the graphical'
+    },
+    {
+        begin: '00:00:10,420',
+        end: '00:00:14,940',
+        script: 'user interface and the operating system for supporting'
+    },
+    {
+        begin: '00:00:14,910',
+        end: '00:00:21,110',
+        script: 'graphical ui has been introduced in the system called Xerox star.'
+    },
+    {
+        begin: '00:00:21,120',
+        end: '00:00:28,840',
+        script: 'So it had a lot of these fantastic ideas that are currently used even after 40 years, right?'
+    },
+    {
+        begin: '00:00:28,850',
+        end: '00:00:35,650',
+        script: 'The idea of icons, folders, and ethernet, file servers, print servers'
+    },
+    {
+        begin: '00:00:35,660',
+        end: '00:00:38,640',
+        script: 'all these ideas came about in Xerox star.'
+    },
+    {
+        begin: '00:00:38,650',
+        end: '00:00:45,360',
+        script: 'And it is known that Steve Jobs stole a lot of the ideas from Xerox.'
+    }
+]
+
+const dictionary = {
+    graphical: [
+        {
+            word: 'graph·i·cal',
+            pronunciation: "| ˈɡrafək(ə)l |",
+            pos: 'adjective',
+            meanings: [
+                {
+                    definition: 'relating to or in the form of a graph',
+                    sentence: 'flowcharts are graphical presentations.'
+                },
+                {
+                    definition: 'relating to visual art or computer graphics',
+                    sentence: 'a high-resolution graphical display.'
+                }
+            ]
+        }
+    ],
+    history: [
+        {
+            word: 'his·to·ry',
+            pronunciation: "| 'hist(ə)rē |",
+            pos: 'noun',
+            meanings: [
+                {
+                    definition: 'the study of past events, particularly in human affairs',
+                    sentence: 'medieval European history.'
+                },
+                {
+                    definition: 'the whole series of past events connected with someone or something',
+                    sentence: 'the history of Aegean painting.'
+                },
+                {
+                    definition: 'a continuous, typically chronological, record of important or public events or of a particular trend or institution',
+                    sentence: 'a history of the labor movement.'
+                }
+            ]
+        }
+    ]
+}
